c5ca21766d060a9ccc4189086180bd9a2fa2c0d3
Cornelius Motanya <corneliusmot@yahoo.com>
2024-08-18 22:16:51 +0300

    changed font-type

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Poppins } from "next/font/google";
+import { Nunito } from "next/font/google";
 import "./globals.css";
 
-const poppins = Poppins({
+const poppins = Nunito({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
 });
@@ -19,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={""}>{children}</body>
+      <body className={poppins.className}>{children}</body>
     </html>
   );
 }
