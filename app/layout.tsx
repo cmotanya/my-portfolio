44e5d2ace1b9078f4629e3c7af3ec1facfa470bf
Cornelius Motanya <corneliusmot@yahoo.com>
2024-08-18 19:30:30 +0300

    changed font style

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Quicksand } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 
-const poppins = Quicksand({
+const poppins = Poppins({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
 });
