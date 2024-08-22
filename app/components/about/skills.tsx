f96730b38c88c5f04a2b0565782878df717b2d4f
Cornelius Motanya <corneliusmot@yahoo.com>
2024-08-22 23:26:34 +0300

    added cards-like for skills

--- app/components/about/skills.tsx
+++ app/components/about/skills.tsx
@@ -29,7 +29,7 @@ const Skills = () => {
         Skills
       </h2>
 
-      <div className="grid grid-cols-1 justify-center gap-8 md:grid-cols-2 lg:grid-cols-3">
+      <div className="grid grid-cols-1 justify-center gap-8 md:grid-cols-2 lg:grid-cols-3 mx-3 md:mx-5">
         {SkillContents.map((skill, index) => (
           <SkillItem key={index} {...skill} />
         ))}
@@ -49,9 +49,12 @@ interface SkillType {
 const SkillItem = ({ name, icon, description, kpi }: SkillType) => {
   return (
     <motion.div
-      whileHover={{ scale: 1.05 }}
+      whileHover={{ scale: 1.05, boxShadow: "0 0 20px rgba(0, 0, 0, 0.15)" }}
       transition={{ duration: 0.2, type: "spring", stiffness: 300 }}
-      className="flex flex-col items-center rounded-lg p-4 shadow-md"
+      className="flex flex-col items-center rounded-lg p-4"
+      style={{
+        boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)",
+      }}
     >
       <Image src={icon} alt="icon" width={50} height={50} className="mb-2" />
       <h3 className="mb-1 font-semibold">{name}</h3>
