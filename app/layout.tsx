224a39e0f6313c174d5015445a3fd78e57fe9d98
Cornelius Motanya <corneliusmot@yahoo.com>
2024-08-22 17:54:53 +0300

    changed font type

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Hind } from "next/font/google";
+import { Spline_Sans } from "next/font/google";
 import "./globals.css";
 
-const nunito = Hind({
+const nunito = Spline_Sans({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
 });
