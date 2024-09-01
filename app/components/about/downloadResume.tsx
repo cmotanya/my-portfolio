54dd5d3855922a4b0a8bc28491f3d8fc12ea27c4
Cornelius Motanya <corneliusmot@yahoo.com>
2024-09-02 00:58:02 +0300

    changed spinner color to black

--- app/components/about/downloadResume.tsx
+++ app/components/about/downloadResume.tsx
@@ -68,7 +68,7 @@ const DownloadResume = () => {
 
 const Spinner = () => (
   <svg
-    className="h-5 w-5 animate-spin text-white"
+    className="h-5 w-5 animate-spin text-black"
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
     viewBox="0 0 24 24"
