5b939f861c4b84be9d39d410bd58dae9f8c62ee3
Cornelius Motanya <corneliusmot@yahoo.com>
2024-09-06 23:24:41 +0300

    changed svg to IconCheck

--- app/components/contact/contact-form.tsx
+++ app/components/contact/contact-form.tsx
@@ -187,26 +187,7 @@ function ContactPage({ accessKey }: { accessKey: string }) {
                   >
                     {/* If is submitting */}
                     {isSubmitting ? (
-                      <svg
-                        className="-ml-1 mr-3 h-5 w-5 animate-spin text-white"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                      >
-                        <circle
-                          className="opacity-25"
-                          cx="12"
-                          cy="12"
-                          r="10"
-                          stroke="currentColor"
-                          strokeWidth="4"
-                        ></circle>
-                        <path
-                          className="opacity-75"
-                          fill="currentColor"
-                          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                        ></path>
-                      </svg>
+                      <IconCheck className="mr-2 h-5 w-5" />
                     ) : (
                       <IconSend className="mr-2 h-5 w-5" stroke={1.5} />
                     )}
@@ -220,7 +201,7 @@ function ContactPage({ accessKey }: { accessKey: string }) {
             {isSubmitSuccessful && isSuccess && (
               <div className="text-center">
                 <IconCheck
-                  className="mx-auto h-12 w-12 text-green-400"
+                  className="mx-auto h-12 w-12 text-green-500"
                   stroke={1.5}
                 />
                 <h3 className="mt-2 text-xl font-medium text-gray-900 dark:text-white">
