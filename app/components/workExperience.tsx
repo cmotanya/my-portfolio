90e0ca16354b88b7abd7b42468c8cc03de67a5d9
Cornelius Motanya <corneliusmot@yahoo.com>
2024-09-08 15:12:57 +0300

    added changes to workExperience

--- app/components/workExperience.tsx
+++ app/components/workExperience.tsx
@@ -2,6 +2,11 @@ import React from "react";
 import { workExperience } from "../lib/workExperience";
 import { cn } from "../utils/cn";
 import { Bebas_Neue } from "next/font/google";
+import {
+  IconHandFingerLeft,
+  IconHandFingerRight,
+  IconPin,
+} from "@tabler/icons-react";
 
 const neue = Bebas_Neue({
   subsets: ["latin"],
@@ -20,36 +25,53 @@ const WorkExperience = () => {
         work experience
       </h3>
 
-      <div className="relative">
-        <div className="absolute left-[0.20rem] h-full w-1 bg-accent md:left-1/2 md:-translate-x-1/2"></div>
+      <div className="md:p-2">
         {workExperience.map((item, index) => (
           <div
             key={index}
             className={cn(
-              "mb-6 ms-4 flex flex-col md:ms-0",
+              "mb-6 ms-6 flex flex-col md:ms-0",
               index % 2 === 0
-                ? "md:me-5 md:items-start"
-                : "md:ms-5 md:items-end",
+                ? "md:me-8 md:items-start"
+                : "md:ms-8 md:items-end",
             )}
           >
-            <div className="absolute left-0 size-2.5 rounded-full bg-primary md:left-1/2 md:-translate-x-1/2"></div>
+            <div
+              className={cn("absolute left-0 md:left-1/2 md:-translate-x-1/2")}
+            >
+              {index % 2 === 0 ? (
+                <span>
+                  <IconHandFingerLeft className="hidden text-primary md:block" />{" "}
+                  <IconHandFingerRight className="text-primary md:hidden" />
+                </span>
+              ) : (
+                <IconHandFingerRight className="text-primary" />
+              )}
+            </div>
 
             <div
-              className={cn(
-                "flex w-full flex-col gap-4 rounded-md border-2 border-secondary p-3 shadow-2xl md:w-1/2",
-              )}
+              className="flex w-full flex-col gap-4 rounded-md bg-100 p-3 shadow-xl md:w-1/2"
+              style={{
+                boxShadow:
+                  "2px 2px 5px rgba(0, 0, 0, 0.1), -2px 2px 5px rgba(0, 0, 0, 0.1), 2px -2px 5px rgba(0, 0, 0, 0.1), -2px -2px 5px rgba(0, 0, 0, 0.1)",
+              }}
             >
               <div className="relative flex justify-between">
-                <span className="uppercase">{item.company}</span>
+                <span className="font-medium uppercase text-600">
+                  {item.company}
+                </span>
                 <span className="rounded-md bg-primary p-1 text-sm text-white">
                   {item.year}
                 </span>
               </div>
 
-              <div className="flex flex-col space-y-4 text-base font-semibold">
+              <div className="flex flex-col space-y-2 text-base font-semibold">
                 <span className="">{item.title}</span>
                 <span className="font-normal">{item.role}</span>
-                <span>{item.location}</span>
+                <span className="flex items-center gap-2 text-sm font-bold text-500">
+                  <IconPin />
+                  {item.location}
+                </span>
               </div>
             </div>
           </div>
